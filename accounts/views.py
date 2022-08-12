46c852e304da24d4bf6924b825c7d0ddca14768d
byObin <byobin26@gmail.com>
2022-08-12 17:20:03 +0900

    회원가입 이메일형식 및 비밀번호 확인

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.contrib.auth.models import User
+from django.http import JsonResponse
+import re
+
 
 #로그인 함수
 def login(request):
@@ -25,8 +28,17 @@ def logout(request):
 #회원가입
 def signup(request):
     if request.method == "POST":
+        # 이메일의 형식에 맞게 입력되었는지 확인
+        if not re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+[.]?\w{2,3}$',(request.POST['username'])):
+                return render(request, 'bad_signup.html')
+                #return JsonResponse({'message':'INVALID_EMAIL_FORMAT'}, status=400)
+        if User.objects.filter(username=request.POST['username']).exists():
+                return JsonResponse({'message':'이미 가입된 이메일입니다. 이전으로 돌아가 다시 시도하세요'}, status=400,  safe=False, json_dumps_params={'ensure_ascii': False})
         if request.POST['password'] == request.POST['repeat']:
             new_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
             auth.login(request, new_user)
             return redirect('home')
+        else:
+            #비밀번호 확인이 일치하지 않는 경우
+            return JsonResponse({'message':'비밀번호 확인이 일치하지 않습니다. 이전으로 돌아가 다시 시도하세요.'}, status=400,  safe=False, json_dumps_params={'ensure_ascii': False})
     return render(request, 'signup.html')
