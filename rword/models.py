d129216979af8ba7e95c5bdd99795d43f2716c43
fmbc <cjungcho@gmail.com>
2022-08-13 11:13:00 +0900

    단어 랜덤

--- rword/models.py
+++ rword/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class WordList(models.Model):
+    word = models.CharField(max_length=64, verbose_name='단어')
+
+    def __str__(self):
+        return self.word
+
+    class Meta:
+        db_table = 'rword'
--- rword/views.py
+++ rword/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+
+from .models import WordList
 
 # Create your views here.
+
 def rwordpage(request):
-    return render(request, 'rwordpage.html')
+    rwordlist = WordList.objects.order_by('?')[:2]
+    return render(request, 'rwordpage.html', {'rwordlist':rwordlist})
