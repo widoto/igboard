215d902fd793f30719880c764a3d9cd5124d6c3a
fmbc <cjungcho@gmail.com>
2022-08-13 11:35:34 +0900

    단어 랜덤

--- rword/views.py
+++ rword/views.py
@@ -9,5 +9,12 @@ from .models import WordList
 # Create your views here.
 
 def rwordpage(request):
-    rwordlist = WordList.objects.order_by('?')[:2]
+    if request.GET.get('button2')=="2":
+        rwordlist = WordList.objects.order_by('?')[:2]
+    elif request.GET.get('button3')=="3":
+        rwordlist = WordList.objects.order_by('?')[:3]
+    elif request.GET.get('button4')=="4":
+        rwordlist = WordList.objects.order_by('?')[:4]
+    else:
+        rwordlist = 'click button'
     return render(request, 'rwordpage.html', {'rwordlist':rwordlist})
