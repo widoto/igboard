4f5df6619a8ca50f1bf27789f89894ca08d1458b
byObin <byobin26@gmail.com>
2022-08-17 09:59:08 +0900

    just commit for pull

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+
+    
 ]
