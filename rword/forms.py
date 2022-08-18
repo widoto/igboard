e3c598fb91c0f57ee22ec61001fc9301da04e1ad
Hajin-Lee0406 <glgkwls@naver.com>
2022-08-18 22:12:28 +0900

    문장생성css

--- rword/forms.py
+++ rword/forms.py
@@ -5,8 +5,26 @@ from django.forms import TextInput
 
 #일반인
 class RSentencesWriteForm(forms.Form):
-    sentence = forms.CharField(error_messages={'required':"문장을 입력하세요"}, label="문장")
-    contents = forms.CharField(error_messages={'required':"문장을 입력하세요"}, widget=forms.Textarea, label='설명')
+    #sentence = forms.CharField(error_messages={'required':"문장을 입력하세요"}, label="문장")
+    #contents = forms.CharField(error_messages={'required':"문장을 입력하세요"}, widget=forms.Textarea, label='설명')
+    sentence = forms.CharField(
+        error_messages={'required':"문장을 입력하세요"},
+        label="문장",
+        widget=forms.TextInput(attrs={
+            'class': 'form-control onesentence',
+            'placeholder': '랜덤 단어를 넣어 한 문장으로 완성하세요.'
+        })
+    )
+    contents = forms.CharField(
+        error_messages={'required':"문장을 입력하세요"},
+        label='설명',
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': '내용을 입력해 주세요.',
+            'rows':"5"
+        })
+
+    )
 
 #댓글
 class SentencesCommentForm(forms.ModelForm):
